2cbc9a36283c4f234fd0176e2771276b64af35db
Alexey <demidow111@gmail.com>
2023-09-08 07:49:54 +0300

    for second task, changed to @ParameterizedTest

--- src/test/java/ru/netology/services/RestScheduleTest.java
+++ src/test/java/ru/netology/services/RestScheduleTest.java
@@ -1,36 +1,18 @@
 package ru.netology.services;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class RestScheduleTest {
 
-    @Test
-    void shouldCalculateForSmallAmounts() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/amounts.csv")
+    void shouldCalculateForAmounts(int income, int expenses, int threshold, int expected) {
         RestSchedule restSchedule = new RestSchedule();
 
-        int income = 10000;
-        int expenses = 3000;
-        int threshold = 20000;
-
-        int expected = 3;
-
         int actual = restSchedule.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void shouldCalculateForLargeAmounts() {
-        RestSchedule restSchedule = new RestSchedule();
-
-        int income = 100000;
-        int expenses = 60000;
-        int threshold = 150000;
-
-        int expected = 2;
-
-        int actual = restSchedule.calculate(income, expenses, threshold);
-
         Assertions.assertEquals(expected, actual);
     }
 }
